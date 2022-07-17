2534a8ed7767b0d7f9ebb473f343526c53d5b10c
KZTanvir <tanir3430@gmail.com>
2022-07-17 20:44:39 +0600

    updated recovered
    recovered from system error

--- create_table/main.sql
+++ create_table/main.sql
@@ -1,8 +1,4 @@
 @drop_all.sql
-@01.create_student.sql
-@02.create_teacher.sql
-@03.create_u_users.sql
-@04.create_groups.sql
-@05.create_msg.sql
-@06.create_notice_board.sql
+@create_all.sql
+@sequences_triggers.sql
 EXIT;  --the script exits here--
